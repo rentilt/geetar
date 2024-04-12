7ac7cfa47eb72c6d58e87258d1aa636d991874ec
rentilt <mrsyder@hotmail.com>
2024-04-12 12:28:25 +0100

    html

--- geetar.py
+++ geetar.py
@@ -7,10 +7,13 @@ try:
         scales = json.load(f)
 except Exception:
     print("Error opening scales.json file...\nPlease run get_scales.py first")
+    exit(-1)
 
 played_notes = ['A#', 'B', 'D#']
 possible_scales = {}
 for name, notes in scales.items():
     if all(x in notes for x in played_notes):
         possible_scales[name] = notes
-print(possible_scales)
+
+print("Played notes: ", played_notes)
+print("Possible Scales: ", list(possible_scales.keys()))
